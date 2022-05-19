45e297c807e5a0c031d66a3d740a011c8cb75b3f
David <popadavid843@yahoo.com>
2022-05-19 19:32:25 +0300

    create card

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 "use strict";
+// close/open modal
+
 const openModal = document.querySelector(".open-modal");
 const closeModal = document.querySelector(".close-question");
 const modal = document.querySelector(".add-question");
@@ -10,3 +12,51 @@ closeModal.addEventListener("click", function (e) {
   e.preventDefault();
   modal.classList.add("display");
 });
+
+//
+
+// take data from user input
+const questionInput = document.getElementById("question-input");
+const answerInput = document.getElementById("question-answer");
+const saveQuestion = document.querySelector(".save-question");
+
+saveQuestion.addEventListener("click", getInput);
+function getInput(e) {
+  e.preventDefault();
+  let q = questionInput.value;
+  let a = answerInput.value;
+  if (a === "" || q === "") alert("Adauga intrebare si raspuns");
+  else {
+    createCard(q, a);
+    // clear(q, a);
+  }
+}
+// const clear = function (q, a) {
+//   q.value = "";
+//   a.value = "";
+// };
+// create card
+const cardCreate = document.querySelector(".cards");
+const createCard = function (a, q) {
+  let card = document.createElement("div");
+  card.classList.add("card");
+  card.innerHTML = `
+  <div class="card-created">
+  <h1 class="question">${a}</h1>
+  <button class="hide">Hide/Answer</button>
+  <h1 class="answer ">${q}</h1>
+  </div>
+  <button class="edit">Edit</button>
+  <button class="remove">Remove</button> 
+  `;
+  cardCreate.appendChild(card);
+
+  let hideBtn = document.querySelector(".hide");
+  let ha = document.querySelector(".answer");
+  ha.style.display = "none";
+
+  hideBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    ha.style.removeProperty("display");
+  });
+};
